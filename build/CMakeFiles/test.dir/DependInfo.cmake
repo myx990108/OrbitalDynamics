e809ccf620c403b8ab65f82aa54b3f36eb8b9232
Miao Yuxuan <myx990108@163.com>
2024-11-21 21:14:43 +0800

    Test for Cmake and Build

---
+++ build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_Fortran
+  "/Users/myx/Desktop/git_test/OrbitalDynamics/drift.f90" "/Users/myx/Desktop/git_test/OrbitalDynamics/build/CMakeFiles/test.dir/drift.f90.o"
+  "/Users/myx/Desktop/git_test/OrbitalDynamics/orbel_xv.f90" "/Users/myx/Desktop/git_test/OrbitalDynamics/build/CMakeFiles/test.dir/orbel_xv.f90.o"
+  "/Users/myx/Desktop/git_test/OrbitalDynamics/test.f90" "/Users/myx/Desktop/git_test/OrbitalDynamics/build/CMakeFiles/test.dir/test.f90.o"
+  )
+set(CMAKE_Fortran_COMPILER_ID "GNU")
+set(CMAKE_Fortran_SUBMODULE_SEP "@")
+set(CMAKE_Fortran_SUBMODULE_EXT ".smod")
+
+# The include file search paths:
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/drift.f90.o"
+  "CMakeFiles/test.dir/orbel_xv.f90.o"
+  "CMakeFiles/test.dir/test.f90.o"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
